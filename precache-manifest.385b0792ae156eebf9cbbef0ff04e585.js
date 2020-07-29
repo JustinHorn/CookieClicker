8977c49062efe20ee768248d9b477927a760012d
y0092888 <ju-horn@web.de>
2020-07-29 12:18:00 +0200

    Updates

---
+++ precache-manifest.385b0792ae156eebf9cbbef0ff04e585.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56b45c201f2a9f628e1af261fd0d3423",
+    "url": "/index.html"
+  },
+  {
+    "revision": "046d310c210a38731cb5",
+    "url": "/static/css/main.f060627e.chunk.css"
+  },
+  {
+    "revision": "a967f29fea78586410df",
+    "url": "/static/js/2.99478f4e.chunk.js"
+  },
+  {
+    "revision": "e46fc7d39878ee64e7b083a156855138",
+    "url": "/static/js/2.99478f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "046d310c210a38731cb5",
+    "url": "/static/js/main.0e0c7f42.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "40d27da8f50a52dcab2e96e17d2ab3a4",
+    "url": "/static/media/cookie.40d27da8.svg"
+  }
+]);
